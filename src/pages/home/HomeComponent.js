3ed3b037edd57be34f264c0a07620e8d1795ad39
charlesy513 <charlesy513@gmail.com>
2024-08-09 23:01:58 -0300

    images to projects and projects on home page

--- src/pages/home/HomeComponent.js
+++ src/pages/home/HomeComponent.js
@@ -4,12 +4,94 @@ import Greeting from "../../containers/greeting/Greeting";
 import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
 
+import GithubRepoCard from "../../components/githubRepoCard/GithubRepoCard";
+import PublicationCard from "../../components/publicationsCard/PublicationCard";
+import Button from "../../components/button/Button";
+import { Fade } from "react-reveal";
+import {
+  greeting,
+  projectsHeader,
+  publicationsHeader,
+  publications,
+} from "../../portfolio.js";
+import ProjectsData from "../../shared/opensource/projects.json";
+import "../projects/Projects.css";
+
 class Home extends Component {
   render() {
+    const theme = this.props.theme;
     return (
       <div>
         <Header theme={this.props.theme} />
         <Greeting theme={this.props.theme} />
+
+        {/* Projects  */}
+        {publications.data.length > 0 ? (
+          <div className="basic-projects">
+            <Fade bottom duration={2000} distance="40px">
+              <div className="projects-heading-div">
+                <div className="projects-heading-text-div">
+                  <h1
+                    className="projects-heading-text"
+                    style={{ color: theme.text }}
+                  >
+                    {projectsHeader.title}
+                  </h1>
+                  <p
+                    className="projects-header-detail-text subTitle"
+                    style={{ color: theme.secondaryText }}
+                  >
+                    {projectsHeader["description"]}
+                  </p>
+                </div>
+              </div>
+            </Fade>
+          </div>
+        ) : null}
+
+        <div className="repo-cards-div-main">
+          {ProjectsData.data.map((repo) => {
+            return <GithubRepoCard repo={repo} theme={theme} />;
+          })}
+        </div>
+        <script><Button
+          text={"More Projects"}
+          className="project-button"
+          href={greeting.githubProfile}
+          newTab={true}
+          theme={theme}
+        /></script>
+
+        {/* Publications  */}
+        {publications.data.length > 0 ? (
+          <div className="basic-projects">
+            <Fade bottom duration={2000} distance="40px">
+              <div className="publications-heading-div">
+                <div className="publications-heading-text-div">
+                  <h1
+                    className="publications-heading-text"
+                    style={{ color: theme.text }}
+                  >
+                    {publicationsHeader.title}
+                  </h1>
+                  <p
+                    className="projects-header-detail-text subTitle"
+                    style={{ color: theme.secondaryText }}
+                  >
+                    {publicationsHeader["description"]}
+                  </p>
+                </div>
+              </div>
+            </Fade>
+          </div>
+        ) : null}
+
+        <div className="repo-cards-div-main">
+          {publications.data.map((pub) => {
+            return <PublicationCard pub={pub} theme={theme} />;
+          })}
+        </div>
+
         <Footer theme={this.props.theme} />
         <TopButton theme={this.props.theme} />
       </div>
